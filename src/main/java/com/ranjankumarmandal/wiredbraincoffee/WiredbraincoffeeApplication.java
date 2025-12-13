bf156138a1e44098f89e0581e1cdd255e2d08550
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-12-13 20:25:38 +0530

    Spring Boot WebFlux: initial setup for my project - Ranjan Wired Brain Coffee

---
+++ src/main/java/com/ranjankumarmandal/wiredbraincoffee/WiredbraincoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.ranjankumarmandal.wiredbraincoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiredbraincoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiredbraincoffeeApplication.class, args);
+	}
+
+}
